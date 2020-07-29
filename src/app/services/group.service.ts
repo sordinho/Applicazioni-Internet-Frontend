83536982c2c6d58114f4d21200581fd27a3dfa99
Davide Sordi <s267570@studenti.polito.it>
2020-07-29 15:42:19 +0200

    working on group proposal gui

--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -4,6 +4,7 @@ import {EMPTY, Observable, of, throwError} from 'rxjs';
 import {Group, TEST_GROUP} from '../models/group.model';
 import {catchError, map} from 'rxjs/operators';
 import {Student} from '../models/student.model';
+import { StudentService } from './student.service';
 
 const httpOptions = {
     headers: new HttpHeaders({
@@ -18,7 +19,7 @@ export class GroupService {
 
     private API_PATH = 'http://localhost:3000/groups';
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient ,private studentService:StudentService) {
     }
 
     getAllGroups(): Observable<Group[]> {
@@ -33,5 +34,8 @@ export class GroupService {
         return of(TEST_GROUP);
     }
 
+    getStudentWithoutGroup(courseID): Observable<Student[]> {
+        return this.studentService.queryAll();
+    }
 
 }
--- src/app/student/groups/groups.component.ts
+++ src/app/student/groups/groups.component.ts
@@ -1,6 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {GroupService} from '../../services/group.service';
 import {Group} from '../../models/group.model';
+import {SelectionModel} from '@angular/cdk/collections';
+import {Student} from '../../models/student.model';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatCheckboxChange} from '@angular/material/checkbox';
+import {MatSort} from '@angular/material/sort';
+import {MatAutocomplete} from '@angular/material/autocomplete';
+import {MatPaginator} from '@angular/material/paginator';
+import {FormControl} from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
     selector: 'app-groups',
@@ -10,19 +19,53 @@ import {Group} from '../../models/group.model';
 export class GroupsComponent implements OnInit {
 
     group: Group = null;
+    selectionModel: SelectionModel<Student> = new SelectionModel<Student>(true, []);
+    dataSource: MatTableDataSource<Student>;
+    colsToDisplay = ['select'].concat('serial', 'name', 'firstName');
+    proposedGroupName = new FormControl();
+    expiryProposal = new FormControl();
+
+    @ViewChild(MatSort, {static: true}) sort: MatSort;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
 
     constructor(private groupService: GroupService) {
     }
 
     ngOnInit(): void {
         this.initStudentGroup();
+        this.initStudentsWithoutGroup();
+
     }
+
 
     initStudentGroup() {
         this.groupService.getStudentGroup('1234', '1')
             .subscribe((data) => {
-                this.group = data;
+                this.group = null; //data;
             });
+    }
+
+    initStudentsWithoutGroup() {
+        this.groupService.getStudentWithoutGroup('1').subscribe(data => {
+            this.dataSource = new MatTableDataSource<Student>(data);
+            this.dataSource.paginator = this.paginator;
+        });
+    }
+
+    toggleTableRow(event: MatCheckboxChange, row: Student) {
+        const ret = this.selectionModel.toggle(row);
+        return ret;
+    }
+
+    proposeGroup() {
+        console.log(this.proposedGroupName.value);
+        console.log(this.expiryProposal.value);
+    }
+
+    disableProposalForm() {
+        // Return true if proposal button need to be disabled
+        return this.proposedGroupName.value === null || this.proposedGroupName.value === '' || this.selectionModel.selected.length === 0 || this.expiryProposal === null
+            || this.expiryProposal.value === '' || !moment(this.expiryProposal.value, 'YYYY-MM-DD',true).isValid()
     }
 
 }
