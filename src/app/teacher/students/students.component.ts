ef295b7971f5b3d09f8a2e3df3fa4a4cdf839f6e
Simone <simone.sinagra96@gmail.com>
2020-07-30 02:17:39 +0200

    table management complete

--- src/app/teacher/students/students.component.ts
+++ src/app/teacher/students/students.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, ViewChild, AfterViewInit, EventEmitter, Output } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource, MatTable } from '@angular/material/table';
 import { Student } from '../../models/student.model';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatCheckboxChange, MatCheckbox } from '@angular/material/checkbox';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-students',
@@ -14,41 +14,38 @@ import { MatPaginator } from '@angular/material/paginator';
 })
 export class StudentsComponent implements AfterViewInit {
 
-  /* array of selectionModel --> the array index is the page (paginator) and have the selected student of that page */  
-  selectionModels: SelectionModel<Student>[] = new Array()  
+  /* student selected - to be added to enrolled student */
+  addStudentSelection: Student = null
+
+  /* selectionModel used to keep track of selected students */
+  selectionModel: SelectionModel<Student> = new SelectionModel<Student>(true, [])
 
   dataSource: MatTableDataSource<Student>
-
-  addStudentSelection: Student = null;
-
+  
   colsToDisplay = ['select'].concat('serial', 'name', 'firstName', 'group');
 
   // ViewChilds
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-  @ViewChild('auto') auto: MatAutocomplete;
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild('masterCheckbox') private masterCheckbox: MatCheckbox;
+  @ViewChild(MatSort, { static: true }) sort: MatSort
+  @ViewChild('auto') auto: MatAutocomplete
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator
+  @ViewChild('masterCheckbox') private masterCheckbox: MatCheckbox
 
   _students: Student[];
   _filteredStudents: Student[];
   _enrolledStudents: Student[];
 
-  // paginator info
-  pageSize: number; 
-  pageIndex: number; 
-
   // component Input interfaces 
   @Input() set students(allStudents: Student[]) {
     this._students = allStudents;
-    this._filteredStudents = allStudents.filter( (student) => student.courseId !== "1" );
+    this._filteredStudents = allStudents.filter( (student) => student.courseId !== "1");
     //console.dir("_filteredStudents" + this._filteredStudents);
   }
   @Input() set enrolledStudents(students: Student[]) {
-    this._enrolledStudents = students;
+    this._enrolledStudents = students
     if(this.dataSource != undefined) {
-      this.dataSource.data = students;
+      this.dataSource.data = students
     } else {
-      this.dataSource = new MatTableDataSource<Student>(students);
+      this.dataSource = new MatTableDataSource<Student>(students)
     }
   }
 
@@ -57,141 +54,129 @@ export class StudentsComponent implements AfterViewInit {
   @Output() removeStudentsEmitter = new EventEmitter<Student[]>();
   
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort
+    this.dataSource.paginator = this.paginator
 
-    this.pageSize = this.paginator.pageSize; 
-    this.pageIndex = this.paginator.pageIndex; 
   }
 
-  toggleTableRow(event: MatCheckboxChange, row: Student) {
-    if(this.selectionModels[this.pageIndex] == null) {
-      // console.dir("creating new selectionModel")
-      // there isn't the selectionModel for the current pageIndex!
-      this.selectionModels[this.pageIndex] = new SelectionModel<Student>(true, [])
-    } else {
-      //console.dir("selectionModel already exists")
-    }
-    return this.selectionModels[this.pageIndex].toggle(row)
+  onSortChanged() {
+    // update data source (sorting)
+    this.dataSource.data = this.dataSource.sortData(this.dataSource.filteredData, this.dataSource.sort)
+    // const shownStudents = this.getShownStudents()
   }
 
-  toggleTableMaster(event: MatCheckboxChange) {
-    if(this.selectionModels[this.paginator.pageIndex] != null && 
-        !this.selectionModels[this.paginator.pageIndex].isEmpty()) {
-      // all the student in the current page are selected --> remove current page students selection
-      this.selectionModels[this.paginator.pageIndex].clear() 
-      if(this.masterCheckbox.checked) this.masterCheckbox.checked = false
+  onPageChanged() {
+    // nothing to do. Only for debug
+    // const shownStudents = this.getShownStudents()
+  }
+
+  getShownStudents() {
+    /* this function return the list of shown students */
+    const startIndex = this.paginator.pageIndex * this.paginator.pageSize
+    const endIndex = startIndex + this.paginator.pageSize
+    const shownStudents = this.dataSource.data.slice(startIndex, endIndex)
+    // console.dir(shownStudents)
+    return shownStudents
+  }
+
+  toggleTableRow(row: Student) {
+    return this.selectionModel.toggle(row);
+  }
+
+  toggleTableMaster() {
+    if(this.getCurrentPageSelectedStudentsNumber() !== 0) {
+      // at least one student in the current page is selected --> remove current page students selection
+      const shownStudents = this.getShownStudents()
+      shownStudents.forEach( student => {
+        if(this.selectionModel.isSelected(student)) {
+          this.selectionModel.toggle(student)
+        }
+      })
+      this.masterCheckbox.checked = false
     } else {
-      this.selectAllPageStudents()
+      /* no students are selected in the current page --> select all */
+      this.selectAllCurrentPageStudents()
     }
   }
 
-  selectAllPageStudents() {
-    var start = this.pageIndex * this.pageSize;
-    var end: number; 
+  getCurrentPageSelectedStudentsNumber() {
+    /* return the number of students selected in the current page */
+    let i: number
+    const shownStudents = this.getShownStudents()
+    let nSelectedStudents: number = 0
+    shownStudents.forEach( (student: Student) => {
+      if(this.selectionModel.isSelected(student)) nSelectedStudents++
+    })
+    
+    return nSelectedStudents
+  }
 
-    if(this.pageIndex == this.paginator.getNumberOfPages()-1) {
-      //console.dir("Sono in ultima pagina");
-      end = this.paginator.length;
-    } else {
-      //console.dir("Non sono in ultima pagina");
-      end = start+this.pageSize;
-    }
+  areAllCurrentPageStudentsSelected() {
+    if(this.dataSource.data.length === 0) return false; // return false if there are no data in the table
 
-    if(this.selectionModels[this.pageIndex] == null) this.selectionModels[this.pageIndex] = new SelectionModel<Student>(true, [])
-    //console.dir("seleziono gli studenti nella pagina corrente (agli indici): ");
-    for(let i=start; i < end; i++) {
-      //console.dir(i);
-      //console.dir("student: " + this.dataSource.data[i].id);
-      this.selectionModels[this.pageIndex].select(this.dataSource.data[i]);
+    /* it returns whether all students in the current page are selected */
+    let shownStudents: Student[] = this.getShownStudents()
+
+    for(let i=0; i<shownStudents.length; i++) {
+      if(!this.selectionModel.isSelected(shownStudents[i])) {
+        // at least one shown student is not selected --> return false!
+        // console.dir("areAllCurrentPageStudentsSelected: false")
+        return false
+      }
     }
+    // all shown students are selected --> return true!
+    // console.dir("areAllCurrentPageStudentsSelected: true")
+    return true
+  }
+
+  areAllStudentsSelected() {
+    if(this.dataSource.data.length === 0) return false; // return false if there are no data in the table
+    // check if all students are selected
+    const data = this.dataSource.data
+    for(let i=0; i<data.length; i++) {
+      if(!this.selectionModel.isSelected(data[i])) {
+        // at least one student is not selected --> return false!
+        // console.dir("areAllStudentsSelected: false")
+        return false
+      }
+    }
+    // all students are selected --> return true!
+    // console.dir("areAllStudentsSelected: true")
+    return true;
+  }
+
+  areStudentsSelected() {
+    /* check if there is at least one student in the selectionModels 
+       --> used to enable/disble delete button in the GUI */
+    return this.selectionModel.hasValue()
+  }
+
+  selectAllCurrentPageStudents() {
+    const shownStudents = this.getShownStudents()
+    shownStudents.forEach((student: Student) => {
+      if(!this.selectionModel.isSelected(student))
+        this.selectionModel.toggle(student)
+      })
   }
 
   selectAllStudents() {
     let i: number
-    let j: number
-    let studentIndex: number = 0
-    var nStudentsPage: number;
-
-
-    for(i=0; i<this.paginator.getNumberOfPages(); i++) {
-      if(this.selectionModels[i] == null) {
-        //console.dir("creating new selectionModel")
-        // there isn't the selectionModel for the current pageIndex!
-        this.selectionModels[i] = new SelectionModel<Student>(true, [])
-      }
-    
-      // compute number of students for each page
-      if(i == this.paginator.getNumberOfPages()-1) {
-        // last page
-        nStudentsPage = this.paginator.length - (this.paginator.getNumberOfPages()-1)*this.paginator.pageSize
-      } else {
-        nStudentsPage = this.paginator.pageSize
-      }
-      
-      for(j=0; j<nStudentsPage; j++) {  
-        if(!this.selectionModels[i].isSelected(this._enrolledStudents[studentIndex])) {
-          //console.dir("selecting stud #" + studentIndex + " firstName: " + this._enrolledStudents[studentIndex].firstName)
-          this.selectionModels[i].toggle(this._enrolledStudents[studentIndex])
-        }
-        studentIndex++;
+    const data = this.dataSource.data
+    for(i=0; i<data.length; i++) {
+      if(!this.selectionModel.isSelected(data[i])) {
+        this.selectionModel.toggle(data[i])
       }
     }
-    //console.dir("all studs sel: " + this.areAllStudentsSelected());
   }
 
   clearAllSelections() {
-    this.selectionModels = []
-  }
-
-  areAllPageStudentsSelected() {
-    // check if all students (in the current page) are selected
-    const nSelected = this.selectionModels[this.pageIndex] != null ? this.selectionModels[this.pageIndex].selected.length : 0  
-    var nStudentsPage: number;
-    
-    if(this.pageIndex == this.paginator.getNumberOfPages()-1) {
-      // last page
-      nStudentsPage = this.paginator.length - (this.paginator.getNumberOfPages()-1)*this.paginator.pageSize
-    } else {
-      nStudentsPage = this.paginator.pageSize
-    }
-    //console.dir("nStudentsPage: " + nStudentsPage);
-    //console.dir("nSelected: " + nSelected)
-    // console.dir("areAllPageStudentsSelected() : " + (nSelected == nStudentsPage))
-
-    return nSelected == nStudentsPage
-  }
-
-  areAllStudentsSelected() {
-    // check if all students are selected
-    let i: number
-    var nSelectedStudents: number = 0
-    for(i=0; i<this.paginator.getNumberOfPages(); i++) {
-      if(this.selectionModels[i] != null && this.selectionModels[i].hasValue()) {
-        nSelectedStudents += this.selectionModels[i].selected.length
-      }
-    }
-
-    return nSelectedStudents == this._enrolledStudents.length
-  }
-
-  areStudentsSelected() {
-    //console.dir("areStudentsSelected()")
-    for(var i=0; i<this.selectionModels.length; i++) {
-      if(this.selectionModels[i] != null && !this.selectionModels[i].isEmpty()) {
-        return true
-      }
-    }
+    this.selectionModel.clear()
   }
 
   removeSelectedRows() {
-    // remove all the student selected in the current page
-    //console.dir("students.ts - studentRemoveEmitter(student: " + this.selectionModel.selected + ")");
-    for(var i = 0; i < this.selectionModels.length; i++) { 
-      this.removeStudentsEmitter.emit(this.selectionModels[i].selected);
-      this.selectionModels[i] = new SelectionModel<Student>(true, []); // reset selectionModel*/
-    }
-
+    // remove all the student selected
+    this.removeStudentsEmitter.emit(this.selectionModel.selected);
+    this.selectionModel.clear()
   }
 
   displayFn(student: Student) {
@@ -218,23 +203,5 @@ export class StudentsComponent implements AfterViewInit {
     // this._students = this._students; // reset options
     this._filteredStudents = this._filteredStudents.filter(student => student !== this.addStudentSelection)
   }
- 
-  pageEvent(event) {
-    /*
-    console.dir("pageEvent(event) ");
-    console.dir("----------");
-    console.dir("(before event) index: " + this.pageIndex + " size: " + this.pageSize)
-    */
-    this.pageIndex = event.pageIndex;
-    this.pageSize = event.pageSize;
-    /*
-    console.dir("(after event) index: " + this.pageIndex + " size: " + this.pageSize)
-    console.dir("----------");
-    console.dir("event.length = " + event.length);
-    console.dir("paginator.length: " + this.paginator.length);
-    console.dir("----------");
-    console.dir("paginator.getNumberOfPages(): " + this.paginator.getNumberOfPages());
-    console.dir("----------");
-    */
-  }
+
 }
